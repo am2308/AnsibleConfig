96936f62a6e0958b1adf203a21e62d8bcad66d7a
am2308 <b110020510@gmail.com>
2020-09-29 18:37:27 +0530

    Adding script to automatically fetch nodes ip list

---
+++ dev/0.0.1/GetClusterNodesIp.py
@@ -0,0 +1,38 @@
+#/bin/python
+
+import sys
+import boto3
+Region = str(sys.argv[1])
+
+#This will create a client of aws ec2 for the specified region
+ec2 = boto3.client('ec2', region_name=Region)
+InstanceList = ['etcd-0', 'controller-0', 'worker-0', 'worker-1', 'worker-2']
+data = ""
+workerList = []
+
+#Above created client will interect with ec2 instances using AWS SDKs based upon filtered instances
+response = ec2.describe_instances()
+
+
+#This is the sample attribute that you can fetch, I have fetched "PublicIp" for worker nodes, etcd, controller and created host file for ansible
+
+
+for reservation in response["Reservations"]:
+    for instance in reservation["Instances"]:
+        for tags in instance['Tags']:
+                if tags['Key'] == 'Name' and tags['Value'] in InstanceList and instance['State']['Name'] == "running":
+                        if tags['Value'].startswith('etcd'):
+                                data = data + "[etcd]\n" + instance['PublicIpAddress'] + "\n"
+                        elif tags['Value'].startswith('cont'):
+                                data = data + "\n[controller]\ncontroller ansible_ssh_host=" + instance['PublicIpAddress'] + "\n"
+                        elif tags['Value'].startswith('worker'):
+                                workerList.append(tags['Value'] + ':' + instance['PublicIpAddress'])
+
+
+data = data + "\n[worker]\n"
+for i in range(0,len(workerList)):
+        data = data + workerList[i].split(':')[0] + " ansible_ssh_host=" + workerList[i].split(':')[1] + "\n"
+
+f = open('groups' ,'w')
+f.write(data)
+f.close()
